83d86b13484e3a0e0bba90cdf09ddf15fbd1c01c
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2021-07-13 12:26:02 -0700

    Initial Commit

---
+++ NBAApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBAApp
+//
+//  Created by Darrick_Truong on 7/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBAApp/NBAAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAAppApp.swift
+//  NBAApp
+//
+//  Created by Darrick_Truong on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct NBAAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
